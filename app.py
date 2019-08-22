01ee726951b41d67705e3e003c859cd54b341ea6
mohamedthariq0705 <53635663+mohamedthariq0705@users.noreply.github.com>
2019-08-22 20:32:20 +0530

    Create app.py
    This code is used to connect the html and python code using FLask

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+import re
+import linkGrabber
+import ast
+
+
+app = Flask(__name__,
+            template_folder="template")
+
+
+@app.route('/')
+def customer():
+    return render_template('web.html')
+a=[]
+
+@app.route('/success', methods=['POST'])
+def print_data():
+    if request.method == 'POST':
+        result = request.form
+        # print(result)
+        links = linkGrabber.Links(result['url'])
+        gb = links.find('href', 'text', pretty=False)
+        # gb= ast.literal_eval(str(gb))
+        # print(type( gb))
+        newlist= []
+        for v in gb:
+            newlist.append({"href":v["href"],"text":v["text"]})
+        print(newlist)
+        return render_template("result.html", result=result,gb=newlist)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
